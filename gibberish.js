df5bacbb18794bee00a084d931f545be1f4f2c8f
Adrian Olaru <agolaru@gmail.com>
2012-03-04 14:37:17 +0200

    exports gibberish

--- gibberish.js
+++ gibberish.js
@@ -1,53 +1,116 @@
-var isNumber = function(number) {
-  return Object.prototype.toString.call(number) == '[object Number]';
-};
+/*!
+ * gibberish
+ * Copyright (c) 2012 Adrian Olaru <agolaru@gmail.com>
+ * MIT Licensed
+ */
 
+ (function() {
 
-//random integer between min and max
-var int = function(min, max){
-  if (!isNumber(min)) min = -10000000;
-  if (!isNumber(max)) max = 10000000;
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+  /**
+   * Gibberish object
+   */
 
-var float = function(min, max, minDigits, maxDigits) {
-  if (!isNumber(min)) min = -10000000;
-  if (!isNumber(max)) max = 10000000;
-  if (!isNumber(minDigits)) minDigits = 2;
-  if (!isNumber(maxDigits)) maxDigits = 4;
-  return +((Math.random() * (max - min) + min).toFixed(int(minDigits, maxDigits)));
-};
+  var g = {};
 
-// id 
-var minId = 0;
-var id = function(min) { 
-  if (isNumber(min)) minId = min; 
-  return minId++;
-};
+  /**
+   * Export the Gibberish object for __Node.js__,  and __Browser__.
+   */ 
 
-//random timestamps
-var timestamp = function() {
-  var now = new Date().getTime(),
-    from1900 = new Date('1900').getTime();
-  return int(from1900, now);
-};
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = g;
+    }
+    exports.gibberish = g;
+  } else {
+    this['gibberish'] = g;
+  }
 
-//random dates
-var date = function() {
-  return new Date(timestamp());
-};
+  /**
+   * Library version.
+   */
 
-//random boolean
-var bool = function() {
-  return !!int(0,1);
-};
+  g.version = '0.1.0';
 
-//guid/uuid
-//http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
-var guid = function() {
-  var s4 = function() {
-    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+  /**
+   * Check if `n` is a number.
+   */
+
+  var isNumber = function(n) {
+    return '[object Number]' == {}.toString.call(n);
   };
-  return (s4()+s4()+"-"+s4()+"-"+s4()+"-"+s4()+"-"+s4()+s4()+s4());
-};
-var uuid = guid;
+
+  /** 
+  * Global counter for id increments
+  */
+
+  var minId = 0;
+
+  /**
+   * Random integer between min and max.
+   */
+
+  g.int = function(min, max){
+    if (!isNumber(min)) min = -10000000;
+    if (!isNumber(max)) max = 10000000;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  /**
+   * Random floats between min and max and with min and max digits
+   */
+
+  g.float = function(min, max, minDigits, maxDigits) {
+    if (!isNumber(min)) min = -10000000;
+    if (!isNumber(max)) max = 10000000;
+    if (!isNumber(minDigits)) minDigits = 2;
+    if (!isNumber(maxDigits)) maxDigits = 4;
+    return +((Math.random() * (max - min) + min).toFixed(g.int(minDigits, maxDigits)));
+  };
+
+  /**
+   * Random timestamps - between 1900 and present
+   */
+
+  g.timestamp = function() {
+    var from = new Date('1900').getTime()
+      , now = new Date().getTime();
+    return g.int(from, now);
+  };
+
+  /**
+   * Random dates.
+   */
+
+  g.date = function() {
+    return new Date(g.timestamp());
+  };
+
+  /**
+   * Random boolean.
+   */
+
+  g.bool = function() {
+    return !!g.int(0,1);
+  };
+
+  /**
+   * Id increments.
+   */
+
+  g.id = function(min) { 
+    if (isNumber(min)) minId = min; 
+    return ++minId;
+  };
+
+  /**
+   * GUID.
+   */
+
+  g.guid = function() {
+    var s4 = function() {
+      return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (s4()+s4()+"-"+s4()+"-"+s4()+"-"+s4()+"-"+s4()+s4()+s4());
+  };
+  g.uuid = g.guid;
+}).call(this);
